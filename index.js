932c467f0a9b356ea3045c80c861a31db09e257f
Maxwell Gottesman <maxgman16@yahoo.com>
2022-09-23 21:45:10 -0400

    commit tree 9d4e587d011acb653c0c409f678c01fb33144ff8

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Follow along with the examples here
+
+function say(toPrint) {
+    console.log(toPrint);
+}
+
+ let number = 3;
+
+ say(`Alright, so you'll be having the ${idSandwich(number)}! That'll be out shortly!`);
+
+ function idSandwich(number) {
+    let sandwich;
+    switch (number) {
+        case 1:
+            sandwich = 'the Dinty Moore';
+            break;
+        case 2:
+            sandwich = 'the Club Sandwich';
+            break;
+        case 3:
+            sandwich = 'the Reuben';
+            break;
+        default:
+            sandwich = `an item that's not on the menu`;
+        }
+    return sandwich;
+    }
